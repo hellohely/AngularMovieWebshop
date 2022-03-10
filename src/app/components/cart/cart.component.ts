066c2bc7d1a6755befdecc5379a785059d5511a0
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-03-10 20:38:13 +0100

    Removed accidental double send-order service

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { AddToCartService } from 'src/app/services/add-to-cart.service';
-import { SendOrderService } from 'src/app/send-order.service';
+import { SendOrderService } from 'src/app/services/send-order.service';
 
 @Component({
   selector: 'app-cart',
--- src/app/services/send-order.service.ts
+++ src/app/services/send-order.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { timeStamp } from 'console';
 
 @Injectable({
   providedIn: 'root'
